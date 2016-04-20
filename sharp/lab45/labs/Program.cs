5a359b6369c9cc1d1072de5252ac64c0f86b51c4
nijjimura <s.y.olshansky@gmail.com>
2016-04-20 23:02:56 +0300

    67

---
+++ sharp/lab45/labs/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exitCode = 0;
+            int x = 0;
+            int y = 0;
+
+            //Лабораторная 4 / 5
+            do //for(;;) { *** if(exitCode != 1) break; } - если нужно через for
+            {
+                Console.WriteLine("Лабораторная 4/5 - Вариант 6");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("z = " + (new LabFourVarSix(x, y, n).getZ()) + "\n Выйти из программы? 1 - выйти");
+                exitCode = int.Parse(Console.ReadLine());
+            } while (exitCode != 1);
+            //////////////////////////////////////////////////////////////////////////////////////////////////////
+        }
+    }
+
+    class LabFourVarSix
+        {
+            public LabFourVarSix(int _x, int _y, int _n)
+            {
+                x = _x;
+                y = _y;
+                n = _n;
+            }
+
+            public double getZ()
+            {
+                double z = 0;
+                for (int inx = 1; inx <= n; ++inx)
+                {
+                    if (inx % 2 == 0)
+                        z += getIteration(inx);
+                    else
+                        z -= getIteration(inx);
+                }
+                return 1 - z;
+            }
+
+            private double getIteration(int iterationValue)
+            {
+                decimal numerator = (ulong)((iterationValue % 2 == 0) ? y * y : x * x);
+                decimal denomerator = getFactorial(iterationValue);
+                double result = (double)(numerator / denomerator);
+                return result;
+            }
+
+            private decimal getFactorial(int iterationValue) //Переполнение возможно
+            {;
+                ulong factorial = 1;
+                for (int inx = 1; inx <= iterationValue + 1; ++inx)
+                {
+                    factorial *= (ulong)inx;
+                }
+                return factorial;
+            }
+
+            public int x;
+            public int y;
+            public int n;
+        }
+
+    } 
---
+++ sharp/labs67/labs/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exitCode = 0;
+            int x = 0;
+            int y = 0;
+
+            //Лабораторная 6/7
+            Console.WriteLine("Лабораторная 6/7 - Вариант 6");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            LabSixVarSix labSix = new LabSixVarSix(x, y);
+            do
+            {
+                labSix.nextIteration();
+                Console.WriteLine("Текущиее значение для итерации" + labSix.getCurrentIterationNumber() + ":" + labSix.getCurrentIterationValue() + "\n Выйти из программы? 1 - выйти / 2 - продолжить");
+                exitCode = int.Parse(Console.ReadLine());
+            } while (exitCode != 1);
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+        }
+    }
+
+    class LabSixVarSix
+        {
+            public LabSixVarSix(int _x, int _y)
+            {
+                x = _x;
+                y = _y;
+                n = 0;
+                p = 0;
+            }
+
+            private double getTermValue(int termNumber) //Вычисляем слагаемое, где termNumber - номер слагаемого
+            {
+                double involutedX = Math.Pow(x, 2 * termNumber - 1);
+                double involutedY = Math.Pow(y, termNumber);
+                double numerator = 0;
+                if(termNumber % 2 == 0)
+                    numerator = involutedX * Math.Cos(involutedY); // РАДИАНЫ! Math.PI * 180 для градусов
+                else
+                    numerator = - involutedX * Math.Sin(involutedY); // РАДИАНЫ! 
+            double denomerator = (2 * termNumber - 1) * (2 * termNumber);
+                double result = numerator/denomerator;
+                return result;
+            }
+
+            public int getCurrentIterationNumber()
+            {
+                return n;
+            }
+
+            public double getCurrentIterationValue()
+            {
+                return p;
+            }
+
+            public void nextIteration()
+            {
+                n++;
+                p += getTermValue(n);
+            }
+
+            private int x;
+            private int y;
+            private int n; //Текущий номер итерации
+            private double p; //Результат работы на текущей итерации
+        }
+}
