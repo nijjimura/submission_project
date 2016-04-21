857c138c93db5f78e5065248ec3ff9437ff9557f
nijjimura <s.y.olshansky@gmail.com>
2016-04-21 17:56:20 +0300

    12

---
+++ sharp/lab12/lab12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная 11 Вариант 6\nВведите количество строк");
+            int i = int.Parse(Console.ReadLine());
+            int[][] mass = new int[i][];
+            Random random = new Random();
+            //Заполнили зубчатый массив
+            for (int inxI = 0; inxI < i; inxI++)
+            {
+                Console.WriteLine("Введите количество элементов для строки {0}", inxI);
+                int j = int.Parse(Console.ReadLine());
+                mass[inxI] = new int[j];
+                for (int inxJ = 0; inxJ < j; inxJ++)
+                {
+                    mass[inxI][inxJ] = random.Next(10);
+                }
+            }
+
+            Console.Write("\n");
+            //Вывели на экран зубчатый массив
+            for (int inxI = 0; inxI < i; inxI++)
+            {
+                int length = mass[inxI].Length;
+                for (int inxJ = 0; inxJ < length; inxJ++) //foreach написать так
+                {
+                    Console.Write("{0}\t", mass[inxI][inxJ]);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
