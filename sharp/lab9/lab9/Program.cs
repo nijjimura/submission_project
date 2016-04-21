c0c6626767ef63cae1b1d1766c65175496878bfc
nijjimura <s.y.olshansky@gmail.com>
2016-04-21 13:59:02 +0300

    lab 9

---
+++ sharp/lab9/lab9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная 9 Вариант 6 \n Введите длину массива");
+            int length = int.Parse(Console.ReadLine());
+            int[] mass = new int[length];
+            Random random = new Random();
+            for(int inx = 0; inx < length; inx++)
+            {
+                int value = random.Next(10);
+                mass[inx] = value;
+                Console.Write(value + "\t");
+            }
+
+            int summ = 0;
+            for(int inx = 0; inx < length; inx++) //Все можно организовать в одном цикле, но тогда нет смысла в массиве ^_^
+            {
+                summ += mass[inx];
+            }
+            Console.WriteLine("\n" + summ);
+            Console.ReadLine();
+        }
+    }
+}
